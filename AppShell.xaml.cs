5e5802d2a9cf177f0face074b07a579806452759
User <User@DESKTOP-5M93885>
2024-10-24 22:49:16 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Syncro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Syncro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
